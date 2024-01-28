19a386b2140dc5f8e7ce35decf40edce8ce1dbe2
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2024-01-28 09:15:05 -0500

    login button created

--- app/src/main/java/com/example/pabodatask2/MainActivity.java
+++ app/src/main/java/com/example/pabodatask2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pabodatask2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        Toast.makeText(view.getContext(), "Form saved successfully!", Toast.LENGTH_SHORT).show();
     }
 }
